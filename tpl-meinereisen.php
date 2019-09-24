b7fcf3b87ec4788d49bf06ce84137713387754f2
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2019-09-24 17:31:32 +0200

    template erstellt

--- tpl-meinereisen.php
+++ tpl-meinereisen.php
@@ -5,44 +5,55 @@ Template Name: MeineReisen
 
 get_header(); ?>
 
-<div>
-
-   <?php if (have_posts()) : while(have_posts()) : the_post();?>
-      <?php the_content();?>
-<?php endwhile;endif;?>
-
-<div class="cool-container">
-
 <?php
-// check if the flexible content field has rows of data
-if( have_rows('first_flexible_content') ):
+   if (have_posts()) :
+      while(have_posts()) : the_post();
+         the_content();
+      endwhile;
+   endif;
+?>
 
-     // loop through the rows of data
-    while ( have_rows('first_flexible_content') ) : the_row();
+<div class="trip">
+<?php
+   // check if the flexible content field has rows of data
+      if( have_rows('trip_flexible') ):
+      // loop through the rows of data
+         while ( have_rows('trip_flexible') ) : the_row();
 
-      if( get_row_layout() == 'text_image' ): ?>
-
-         <div class="cool-text">
-            <?php the_sub_field('text'); ?>
-         </div>
-         <div class="cool-image">
-            <?php
-               $img = get_sub_field('image');
-            ?>
-            <img src="<?php echo $img['url']; ?>" alt="">
-         </div>
-
-         <?php
-        endif;
-
-    endwhile;
-
-else :
-
-    // no layouts found
-
-endif; ?>
-</div>
+            if( get_row_layout() == 'layout_flexible' ):
+?>
+               <div class="trip__heading">
+                  <?php the_sub_field('trip_heading'); ?>
+               </div>
+               <div class="trip__image">
+                  <?php
+                     $tripImg = get_sub_field('trip_thumb');
+                  ?>
+                  <img src="<?php echo $tripImg['url']; ?>" alt="">
+               </div>
+               <div class="trip__description">
+                  <?php the_sub_field('trip_description'); ?>
+               </div>
+<?php
+            endif;
+            if( get_row_layout() == 'layout_img_and_text' ):
+?>
+               <div class="trip__image">
+                  <?php
+                     $tripImg = get_sub_field('trip_img');
+                  ?>
+                  <img src="<?php echo $tripImg['url']; ?>" alt="">
+               </div>
+               <div class="trip__description">
+                  <?php the_sub_field('trip_img_descr'); ?>
+               </div>
+<?php
+            endif;
+         endwhile;
+      else :
+      // no layouts found
+      endif;
+?>
 </div>
 
 <?php get_footer();?>
