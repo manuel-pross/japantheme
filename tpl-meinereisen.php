3955a44c7a1b287f799f750997f079e6a711c410
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2019-09-26 13:18:16 +0200

    Css für picture left und right hinzugefügt,

--- tpl-meinereisen.php
+++ tpl-meinereisen.php
@@ -19,32 +19,38 @@ get_header(); ?>
       // loop through the rows of data
          while ( have_rows('trip_flexible') ) : the_row();
 
-            if( get_row_layout() == 'layout_flexible' ):
+            if( get_row_layout() == 'layout_thumbnail' ):
 ?>
-               <div class="trip-thumbnail">
-                  <div class="trip-thumbnail__container">
-                     <h2 class="trip-thumbnail__heading"><?php the_sub_field('trip_heading'); ?></h2>
+               <div class="thumbnail">
+                  <figure class="thumbnail__container">
                      <?php
-                        $tripImg = get_sub_field('trip_thumb');
+                        $tripImg = get_sub_field('img');
                      ?>
-                     <img src="<?php echo $tripImg['url']; ?>" alt="<?php echo $tripImg['alt']; ?>" class="trip-thumbnail__image">
-                     <p class="trip-thumbnail__description"><?php the_sub_field('trip_description'); ?></p>
+                     <img src="<?php echo $tripImg['url']; ?>" alt="<?php echo $tripImg['alt']; ?>" class="thumbnail__image">
+                     <figcaption class="thumbnail__caption">
+                        <h2 class="thumbnail__heading"><?php the_sub_field('heading'); ?></h2>
+                     </figcaption>
+                  </figure>
+                  <p class="thumbnail__description"><?php the_sub_field('descr'); ?></p>
                   </div>
-               </div>
 <?php
             endif;
             if( get_row_layout() == 'layout_imgs_and_text' ):
 ?>
                <div class="trip-heading-imgs-descr">
-                  <h2 class="trip-heading-imgs-descr__heading"><?php the_sub_field('trip_imgs_heading'); ?></h2>
+                  <h3 class="trip-heading-imgs-descr__heading"><?php the_sub_field('trip_imgs_heading'); ?></h3>
                   <?php $tripImgs = get_sub_field('trip_imgs_gallery'); ?>
                   <?php
                      $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                     $counter = 1;
                      if( $tripImgs ): ?>
                         <ul class="trip-heading-imgs-descr__images-container">
                            <?php foreach( $tripImgs as $image ): ?>
                                  <li class="trip-heading-imgs-descr__image-container">
-                                    <?php echo wp_get_attachment_image( $image['ID'], $size, false,  array( "class" => "trip-heading-imgs-descr__image" ) ); ?>
+<?php
+                                    echo wp_get_attachment_image( $image['ID'], $size, false,  array( "class" => "trip-heading-imgs-descr__image number" . $counter) );
+                                    $counter++;
+?>
                                  </li>
                            <?php endforeach; ?>
                         </ul>
@@ -52,6 +58,41 @@ get_header(); ?>
                      <p class="trip-heading-imgs-descr__description"><?php the_sub_field('trip_imgs_description'); ?></p>
                </div>
 <?php
+            endif;
+            if( get_row_layout() == 'layout_img_text' ):
+               if(get_sub_field('toggle')):
+?>
+                  <div class="picture-left">
+                     <div class="picture-left__container">
+                           <h3 class="picture-left__heading"><?php the_sub_field('heading'); ?></h3>
+                           <p class="picture-left__description"><?php the_sub_field('descr'); ?></p>
+                     </div>
+                     <?php $img = get_sub_field('img');?>
+                        <figure class="picture-left__figure">
+                           <img src="<?php echo $img['url']; ?>" alt="<?php echo $tripimg['alt']; ?>" class="picture-left__image">
+                           <figcaption class="picture-left__caption-container">
+                              <span class="picture-left__caption"><?php echo $img['caption']; ?></span>
+                           </figcaption>
+                        </figure>
+                  </div>
+<?php
+               else:
+?>
+                  <div class="picture-right">
+                     <div class="picture-right__container">
+                        <h3 class="picture-right__heading"><?php the_sub_field('heading'); ?></h3>
+                        <p class="picture-right__description"><?php the_sub_field('descr'); ?></p>
+                     </div>
+                     <figure class="picture-right__figure">
+                        <?php $img = get_sub_field('img');?>
+                        <img src="<?php echo $img['url']; ?>" alt="<?php echo $tripimg['alt']; ?>" class="picture-right__image">
+                        <figcaption class="picture-right__caption-container">
+                           <span class="picture-right__caption"><?php echo $img['caption']; ?></span>
+                        </figcaption>
+                     </figure>
+                  </div>
+<?php          endif;
+
             endif;
          endwhile;
       else :
